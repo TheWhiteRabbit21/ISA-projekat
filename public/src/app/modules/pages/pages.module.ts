b2178b9e08b5f1cf47ca309ba28d43a37aaebccf
Tomislav Andric <tomislavandric11@gmail.com>
2022-11-11 19:29:10 +0100

    editing data partially done

--- public/src/app/modules/pages/pages.module.ts
+++ public/src/app/modules/pages/pages.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from 'src/app/app-routing.module'; 
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
   imports: [
     CommonModule,
     AppRoutingModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
--- public/src/app/modules/pages/user-profile/user-profile.component.ts
+++ public/src/app/modules/pages/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -7,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  user  = {
+    name: "Milos",
+    surname: "Obilic",
+    id:"0304000230023",
+    gender:"male",
+    email:"asdf@gmail.com",
+    address:"ulica 12",
+    city:"Paracin",
+    state:"Srbija",
+    phone:"0611234123",
+    points:"3",
+    category:"2"
+  };
+
+  editedUser = Object.assign({}, this.user);
   constructor() { }
 
   ngOnInit(): void {
     //ovde dobavis podatke iz bekenda
+
+
+    // console.log(user);
   }
 
   public onOpenModal( mode: string): void {
@@ -29,5 +47,11 @@ export class UserProfileComponent implements OnInit {
 
     button.click();
   }
+
+  public saveButton(): void {
+    this.user = Object.assign({}, this.editedUser);
+
+  }
+
 
 }
