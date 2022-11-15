1bece28cead2a43f9589dc4a9a848ebee50aa2e0
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-11-15 12:57:10 +0100

    Update import.sql

--- projekat/src/main/resources/import.sql
+++ projekat/src/main/resources/import.sql
@@ -2,4 +2,4 @@ insert into blood_bank_center (name, adress, description, average_rating) values
 insert into blood_bank_center (name, adress, description, average_rating) values ('Mika', 'bul cara Lazara', 'Opis neki', '4');
 insert into blood_bank_center (name, adress, description, average_rating) values ('Djura', 'bul Mihajla Pupina', 'Opis nesto', '4.99');
 insert into blood_bank_center (name, adress, description, average_rating) values ('Zika', 'bul Avnoja', 'Opis neki neki', '2.3');
-insert into blood_bank_center (name, adress, description, average_rating) values ('Kica', 'bul patrijarha Pavla', 'Opis nesto nesto nesto', '3');
+insert into blood_bank_center (name, adress, description, average_rating) values ('Kica', 'bul patrijarha Pavla', 'Opis nesto nesto', '3');
