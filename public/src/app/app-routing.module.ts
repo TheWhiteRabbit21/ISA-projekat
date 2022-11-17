c308c2114494c5658105356995e11c4cc3c44509
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-11-17 10:51:32 +0100

    Update app-routing.module.ts

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register-center', component: RegisterCenterComponent },
   { path: 'register-center-admin', component: RegisterCenterAdminComponent },
-  {path: 'userProfile', component: UserProfileComponent},
+  { path: 'userProfile', component: UserProfileComponent},
   { path: 'adminprofile', component: AdminProfileComponent },
   { path: 'admincenterprofile/:id', component: AdminCenterProfileComponent },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
