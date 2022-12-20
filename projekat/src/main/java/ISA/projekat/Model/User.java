944b0c8ea22303c6f9840697f99f2b5c0964e18d
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2022-12-20 20:29:30 +0100

    fixed minor issues

--- projekat/src/main/java/ISA/projekat/Model/User.java
+++ projekat/src/main/java/ISA/projekat/Model/User.java
@@ -120,6 +120,14 @@ public class User implements Serializable, UserDetails{
         this.email = email;
     }
     
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 	public String getPassword() {
 		return password;
 	}
