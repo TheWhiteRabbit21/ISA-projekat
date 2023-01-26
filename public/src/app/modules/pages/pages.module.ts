80ad8ddd7f6bb48a92581edfefdeaadcf108054e
Tomislav Andric <tomislavandric11@gmail.com>
2023-01-26 13:44:37 +0100

    login

--- public/src/app/modules/pages/pages.module.ts
+++ public/src/app/modules/pages/pages.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AdminProfileComponent } from './admin-profile/admin-profile.component';
 import { AdminCenterProfileComponent } from '../admin-center/pages/admin-center-profile/admin-center-profile.component';
 import { MatTableModule } from '@angular/material/table';
@@ -15,13 +14,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BloodDonorFormComponent } from './blood-donor-form/blood-donor-form.component';
 import { LoginComponent } from './login/login.component';
 import { BloodDonorsListComponent } from './blood-donors-list/blood-donors-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { RegisteredUsersListComponent } from '../admin/pages/registered-users-list/registered-users-list.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    UserProfileComponent,
     AdminProfileComponent,
+    UserProfileComponent,
     AdminCenterProfileComponent,
     WelcomeComponent,
     RegisterComponent,
