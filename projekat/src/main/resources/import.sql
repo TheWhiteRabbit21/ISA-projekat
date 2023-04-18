f591a19301e90c0f2ca22251f37b9168233d59d1
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2023-04-18 16:59:39 +0200

    css update and backend fixed issue

--- projekat/src/main/resources/import.sql
+++ projekat/src/main/resources/import.sql
@@ -17,6 +17,11 @@ insert into blood_bank_center (name, address_id, description, average_rating) va
 insert into blood_bank_center (name, address_id, description, average_rating) values ('Alucard', 8, 'Najprijatniji radnici.', '4.99');
 insert into blood_bank_center (name, address_id, description, average_rating) values ('Dracula', 9, 'Stari načini vađenja krvi se vraćaju u modu.', '2.3');
 insert into blood_bank_center (name, address_id, description, average_rating) values ('Krpelj', 10, 'Divan banka krvi omeđena prostranim livadama.', '3');
+insert into blood_bank_center (name, address_id, description, average_rating) values ('Elektra', 6, 'Divan banka krvi omeđena prostranim livadama.', '3');
+insert into blood_bank_center (name, address_id, description, average_rating) values ('Kod Rocka', 7, 'Divan banka krvi omeđena prostranim livadama.', '3');
+insert into blood_bank_center (name, address_id, description, average_rating) values ('Epruveta', 8, 'Divan banka krvi omeđena prostranim livadama.', '3');
+insert into blood_bank_center (name, address_id, description, average_rating) values ('Divna', 9, 'Divan banka krvi omeđena prostranim livadama.', '3');
+insert into blood_bank_center (name, address_id, description, average_rating) values ('Benu', 10, 'Divan banka krvi omeđena prostranim livadama.', '3');
 
 
 insert into roles (name) values ('ROLE_USER');
