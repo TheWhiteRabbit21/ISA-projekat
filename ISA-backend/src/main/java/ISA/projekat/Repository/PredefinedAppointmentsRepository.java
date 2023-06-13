940ba9a06b375ec6ac3d1f3cae784750ea5cf8d8
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2023-06-13 19:53:55 +0200

    Created model and repository
    Predefined Appointments with neccessery columns

---
+++ ISA-backend/src/main/java/ISA/projekat/Repository/PredefinedAppointmentsRepository.java
@@ -0,0 +1,7 @@
+package ISA.projekat.Repository;
+
+import ISA.projekat.Model.PredefinedAppointments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PredefinedAppointmentsRepository extends JpaRepository<PredefinedAppointments, Integer> {
+}
